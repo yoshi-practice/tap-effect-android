7d9cdd3fece4e98a97a3fbb422ccd0602392c3fa
yoshi (Yoshihisa Kaino) <yoppy1125yoppy@gmail.com>
2019-02-21 20:38:08 +0900

    [Update] MainActivity

--- app/src/main/java/com/yoshi1125hisa/tapeffect/MainActivity.java
+++ app/src/main/java/com/yoshi1125hisa/tapeffect/MainActivity.java
@@ -28,31 +28,24 @@ public class MainActivity extends AppCompatActivity {
         float pointX = event.getX();
         float pointY = event.getY();
 
-//画面dpを取得して画像サイズを準備
         float dp = getResources().getDisplayMetrics().density;
         int imgSize = (int) (50 * dp);
 
-//画像を設定
         final ImageView img = new ImageView(getApplicationContext());
         img.setImageResource(R.drawable.ic_launcher_background);
 
-//ImageViewに画像サイズを設定
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(imgSize, imgSize);
         img.setLayoutParams(params);
 
-//画像の真ん中にタッチポイントが来るように設定
         img.setX(pointX - imgSize / 2);
         img.setY(pointY - imgSize / 2);
         img.setScaleType(ImageView.ScaleType.FIT_XY);
 
-//画像を半分のサイズに縮小
         img.setScaleX(0.5f);
         img.setScaleY(0.5F);
 
-//画像を描画
         tapLayout.addView(img);
 
-//0.3秒かけて元のサイズに拡大しながら透明になるアニメーション
         ViewCompat.animate(img)
                 .setDuration(300)
                 .alpha(0)
@@ -66,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onAnimationEnd(View view) {
-//アニメーション終了時に削除
                         tapLayout.removeView(img);
                     }
 
